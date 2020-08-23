5cf569bb1ebec7239a158dba291042280a0c21e0
poorting <remco@poortinga-vanwijnen.nl>
2020-08-23 15:18:17 +0200

    added --noinput to collectstatic to avoid having to have user input

--- housekeeping/postupdate.sh
+++ housekeeping/postupdate.sh
@@ -1,3 +1,3 @@
 cp -R /home/ddosdb/.ddosdb/ddosdb/ddosdb/. /opt/ddosdb/.
-python3 /opt/ddosdb/manage.py collectstatic
+python3 /opt/ddosdb/manage.py collectstatic --noinput
 sudo systemctl restart apache2.service
