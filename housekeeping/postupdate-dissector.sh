d503e76a20bc82b782c6aa50a5ebfc29dbf06c82
João Marcelo Ceron <joaoceron@users.noreply.github.com>
2020-08-26 11:36:33 -0300

    Create postupdate-dissector.sh

---
+++ housekeeping/postupdate-dissector.sh
@@ -0,0 +1,3 @@
+cp -R -n ./../../ddos_dissector ~/ddos_dissector/
+cp -f  ./../../ddos_dissector/ddos_dissector.py  ~/ddos_dissector/ddos_dissector.py
+sudo cp -f ~/ddos_dissector/ddos_dissector.py /usr/bin/
