8f7c9b9ebe657c13739a34a98a1f66954195b3f6
Remco Poortinga - van Wijnen <remco.poortinga@surfnet.nl>
2020-12-01 13:57:49 +0100

    Install module via sudo pip3 to prevent it getting only in python lib for ddosdb user.

--- housekeeping/postupdate.sh
+++ housekeeping/postupdate.sh
@@ -1,6 +1,6 @@
+sudo pip3 install django-searchable-encrypted-fields
 cp -R /home/ddosdb/.ddosdb/ddosdb/ddosdb/. /opt/ddosdb/.
 python3 /opt/ddosdb/manage.py collectstatic --noinput
 python3 /opt/ddosdb/manage.py makemigrations
 python3 /opt/ddosdb/manage.py migrate
-pip install django-searchable-encrypted-fields
 sudo systemctl restart apache2.service
