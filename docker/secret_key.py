dfb96a047e809d51b14976cdcf25e7dbfe86d344
João Marcelo Ceron <joaoceron@users.noreply.github.com>
2021-02-22 20:42:54 -0300

    Update secret_key.py
    fix secret_key generation

--- docker/secret_key.py
+++ docker/secret_key.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 import random;
 
-print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]), end="")
+#print("".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]), end="")
+result = "".join([random.choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for i in range(50)]); print(result)
