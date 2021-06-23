950295a1ecd4c8068b91d9944d80293f8db86111
poorting <remco.poortinga@surf.nl>
2021-06-23 06:00:33 +0200

    Fixed Django container waiting for Postgres db

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+echo "Waiting for postgres... ($SQL_HOST:$SQL_PORT)"
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+while ! nc -z $SQL_HOST $SQL_PORT; do
+  sleep 1
+done
 
-    echo "PostgreSQL started"
-fi
-
-# python manage.py flush --no-input
-# python manage.py migrate
+echo "PostgreSQL started"
 
 exec "$@"
