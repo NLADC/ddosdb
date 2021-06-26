e6086df2fbeb659e0367726bc3b69524ed4126b5
poorting <remco.poortinga@surf.nl>
2021-06-26 13:36:47 +0200

    Applied waiting for db fix from master branch

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-if [-v POSTGRES_DB ]
-then
-  echo "Waiting for postgres... ($SQL_HOST:$SQL_PORT)"
-  while ! nc -z $SQL_HOST $SQL_PORT; do
-    sleep 1
-  done
-  echo "PostgreSQL started"
-fi
+echo "Waiting for postgres... ($SQL_HOST:$SQL_PORT)"
+while ! nc -z $SQL_HOST $SQL_PORT
+do
+  sleep 1
+done
+echo "PostgreSQL started"
+
 exec "$@"
