41dba0b959c3bf64dcc7c7eb02f7aa5248fff0bc
poorting <remco.poortinga@surfnet.nl>
2021-08-04 11:42:51 +0200

    Don't delete old sacheduled tasks if they already exist

--- ddosdb/ddosdb/management/commands/celery.py
+++ ddosdb/ddosdb/management/commands/celery.py
@@ -25,24 +25,47 @@ class Command(BaseCommand):
                 i += 1
                 logger.info("Periodic Task {}: {}".format(i, pt))
 
-            logger.info("Deleting all ddosdb tasks")
-            PeriodicTask.objects.filter(task='ddosdb.tasks.check_to_sync').delete()
-            PeriodicTask.objects.filter(task='ddosdb.tasks.push_sync').delete()
-            PeriodicTask.objects.filter(task='ddosdb.tasks.pull_sync').delete()
+            # logger.info("Deleting all ddosdb tasks")
+            # PeriodicTask.objects.filter(task='ddosdb.tasks.check_to_sync').delete()
+            # PeriodicTask.objects.filter(task='ddosdb.tasks.push_sync').delete()
+            # PeriodicTask.objects.filter(task='ddosdb.tasks.pull_sync').delete()
 
             logger.info("Creating the default ddosdb push/pull sync tasks")
+            IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.MINUTES)
+            IntervalSchedule.objects.get_or_create(every=15, period=IntervalSchedule.SECONDS)
             schedule, created = IntervalSchedule.objects.get_or_create(
                 every=1, period=IntervalSchedule.HOURS)
-            PeriodicTask.objects.get_or_create(
-                interval=schedule,
+
+            # Create Pull sync if needed
+            p_task, created = PeriodicTask.objects.get_or_create(
                 name='Remote pull sync',  # Description
                 task='ddosdb.tasks.pull_sync',  # name of task.
+                defaults={'interval': schedule}
             )
-            PeriodicTask.objects.get_or_create(
-                interval=schedule,
+            if created:
+                logger.info("Pull sync task created")
+            else:
+                logger.info("Pull sync task already exists")
+            logger.info("with scheduled interval of: {}".format(p_task.interval))
+
+            # Create Push sync if needed
+            p_task, created = PeriodicTask.objects.get_or_create(
                 name='Remote push sync',  # Description
                 task='ddosdb.tasks.push_sync',  # name of task.
+                defaults={'interval': schedule}
             )
+            if created:
+                logger.info("Push sync task created")
+            else:
+                logger.info("Push sync task already exists")
+            logger.info("with scheduled interval of: {}".format(p_task.interval))
+
+
+            # PeriodicTask.objects.get_or_create(
+            #     interval=schedule,
+            #     name='Remote push sync',  # Description
+            #     task='ddosdb.tasks.push_sync',  # name of task.
+            # )
 
             # Delete all backup cleaning tasks and create a new one
             # logger.info("Deleting all celery.backend_cleanup tasks")
