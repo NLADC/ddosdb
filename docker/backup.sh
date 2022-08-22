9391e8e6814bd170532ead2cd557986e0c5419fe
tvdhout <thijsvandenhout@live.nl>
2022-08-22 15:08:03 +0200

    Backup and restore ddosdb contents too

--- docker/backup.sh
+++ docker/backup.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env sh
 
+now="$(date +'%Y%m%d%H%M')"
+
+mkdir "backup_${now}"
+mkdir "backup_${now}/nginx"
+
 # Copy NGINX conf files
-docker cp ddosdb_nginx:/etc/nginx/conf.d/ etc/.
+docker cp ddosdb_nginx:/etc/nginx/conf.d/ "backup_${now}/nginx/".
 
 # Copy Let's Encrypt settings and certificates
-docker cp ddosdb_nginx:/etc/letsencrypt/ etc/.
+docker cp ddosdb_nginx:/etc/letsencrypt/ "backup_${now}/nginx/".
 
+# Dump DDoS-DB postgres database with users
+docker exec ddosdb_db pg_dump -h localhost -U ddosdb -F c -b -f "/tmp/postgres_backup.sql"
+docker cp ddosdb_db:/tmp/postgres_backup.sql "backup_${now}"
 
+# Dump MongoDB with fingerprints
+docker exec ddosdb_mongo mongodump > /dev/null 2> /dev/null
+docker cp ddosdb_mongo:/dump "backup_${now}/mongodb"
--- docker/restore.sh
+++ docker/restore.sh
@@ -4,7 +4,13 @@ COL='\033[0;37m'
 RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-printf "${COL}\n Are you sure you want to restore certificates and NGINX configurations? [y/N]:${NC}"
+BACKUP=$1
+if [ ! -d "$BACKUP" ] || [ ! -d "$BACKUP/nginx" ] || [ ! -d "$BACKUP/mongodb" ] || [ ! -f "$BACKUP/postgres_backup.sql" ]; then
+  echo "$BACKUP is not a path to a valid DDoS-DB backup"
+  return 1
+fi
+
+printf "${COL}\n Are you sure you want to restore this backup and remove any existing content? [y/N]:${NC}"
 read restore
 
 if [ x"$restore" = x"y" ]
@@ -14,5 +20,18 @@ then
 
   # Copy Let's Encrypt settings and certificates
   docker cp etc/letsencrypt/ ddosdb_nginx:/etc/.
+
+
+  # Dump DDoS-DB postgres database with users
+  docker cp "$BACKUP/postgres_backup.sql" ddosdb_db:/tmp/postgres_backup.sql
+  docker exec ddosdb_db psql -U ddosdb -d postgres -c "drop database ddosdb" > /dev/null
+  docker exec ddosdb_db psql -U ddosdb -d postgres -c "create database ddosdb" > /dev/null
+  docker exec ddosdb_db pg_restore -h localhost -U ddosdb -d ddosdb "/tmp/postgres_backup.sql"
+
+  # Dump MongoDB with fingerprints
+  docker cp "$BACKUP/mongodb" ddosdb_mongo:/dump
+  docker exec ddosdb_mongo mongorestore > /dev/null 2> /dev/null
+
 fi
 
+echo "Backup restored succesfully"
